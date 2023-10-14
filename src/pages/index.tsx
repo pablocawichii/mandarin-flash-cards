d6779ec33a0000d94aa7a6a5bdb0f57a83c568fe
Pablo Cawich II <pablocawichii@gmail.com>
2023-10-14 01:33:16 -0600

    Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.randomWord.useQuery();
 
   return (
     <>
@@ -43,7 +43,7 @@ export default function Home() {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {hello.data ? hello.data.character : "Loading tRPC query..."}
           </p>
         </div>
       </main>
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,16 @@
 import { z } from "zod";
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from "csv-parse";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+type Word = {
+  pinyin: string;
+  noTone: string;
+  character: string;
+  meaning: string;
+};
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -12,5 +22,34 @@ export const exampleRouter = createTRPCRouter({
     }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.example.findMany();
+  }),
+  randomWord: publicProcedure.query(({}) => {
+
+    const headers = ["pinyin", "noTone", "character", "meaning"];
+
+    // const fileContent = fs.readFileSync("", { encoding: "utf-8" });
+
+    // const promise = await new Promise<Word[]>((res, rej) => {
+    //   parse(
+    //     fileContent,
+    //     {
+    //       delimiter: ",",
+    //       columns: headers,
+    //     },
+    //     (error, result: Word[]) => {
+    //       if (error) {
+    //         rej(error);
+    //       }
+    //       res(result);
+    //     },
+    //   );
+    // });
+
+    
+
+    // return promise[Math.floor(Math.random() * promise.length)];
+    return {
+      character: ""
+    };
   }),
 });
